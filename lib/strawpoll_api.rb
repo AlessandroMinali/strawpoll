cf5b8808a4452ef2414575e7601362ca5652eba1
Alessandro Minali <alessandro.minali@gmail.com>
2015-06-08 19:43:23 -0400

    strawpoll

--- lib/strawpoll_api.rb
+++ lib/strawpoll_api.rb
@@ -3,14 +3,15 @@ require 'net/http'
 
 class StrawPoll
 	attr_accessor :title, :multi, :perm, :options 
-	attr_reader :api, :id, :leader, :votes, :link
+	attr_reader :api, :id, :leader, :votes, :link, :results
 
 	def initialize title, *options
+		raise ArgumentError unless options.length >= 2
 		@title = title
 		@multi = false
 		@perm = true
 		@options = options
-		@api = 'http://strawpoll.me/api/v2/'
+		@api = 'http://strawpoll.me/api/v2/polls/'
 		@id = nil
 		@leader = nil
 		@votes = nil
@@ -19,16 +20,16 @@ class StrawPoll
 	end
 
 	def create!
-		raise ArgumentError unless !@title.empty? && @options.length >= 2 && @mulit.is_a?(Boolean) && @perm.is_a?(Boolean)
+		raise ArgumentError unless !@title.empty? && @options.length >= 2
 		params = {
 			"title" => @title,
 			"options" => @options,
 			"multi" => @multi,
 			"permissive" => @perm,
 		}
-		url = URI 'http://strawpoll.me/api/v2/polls'
+		url = URI @api
 		resp = Net::HTTP.post_form(url, params)
-		@id = JSON.parse(resp.body)['id'].to_s
+		@id = JSON.parse(resp.body)['id']
 		@link = "http://strawpoll.me/#{@id}" 
 		@results = "http://strawpoll.me/#{@id}/r"
 		resp.body
@@ -36,14 +37,13 @@ class StrawPoll
 
 	def view id=@id
 		raise ArgumentError unless id.is_a? Integer
-		url = URI "http://strawpoll.me/api/v2/#{id}"
+		url = URI "#{@api}#{id}"
 		resp = Net::HTTP.get(url)
-		resp.body
 	end
 
 	def winner id=@id
 		raise ArgumentError unless id.is_a? Integer
-		url = URI "http://strawpoll.me/api/v2/#{id}"
+		url = URI "#{@api}#{id}"
 		resp = Net::HTTP.get(url)
 		result = JSON.parse(resp)['votes']
 		winner = JSON.parse(resp)['options'][result.index(result.max)]
